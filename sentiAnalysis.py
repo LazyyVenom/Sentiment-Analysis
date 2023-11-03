15a6c022c7f66d6298145c9bd28ee63eaeca9f79
Anubhav <choubey.anubhav253@gmail.com>
2023-11-03 19:07:32 +0530

    added old program codes

---
+++ sentiAnalysis.py
@@ -0,0 +1,13 @@
+### VIDEO -> Images & Audio -> Audio -> NLP -> Text -> Emotions
+###       -> Images -> CV -> Emotions
+
+from moviepy.editor import VideoFileClip
+
+def convert_video_to_wav(input_video, output_wav):
+    video_clip = VideoFileClip(input_video)
+    audio_clip = video_clip.audio
+
+    # Save the audio as a WAV file
+    audio_clip.write_audiofile(output_wav, codec='pcm_s16le')
+
+convert_video_to_wav(r'chotu motu.mp4','out.wav')
